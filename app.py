233c20761b75cfb9b6bd37f2349b80dd2912d6b2
Wizxrdx <benedick.dumol@gmail.com>
2024-10-06 10:14:58 +0000

    added: post handler for notification

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 from src.utils import WRS2, LandsatAcquisition
 import io
 import matplotlib.pyplot as plt
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+
 schedule = LandsatAcquisition()
 wrs2 = WRS2()
 
@@ -27,14 +31,38 @@ def plot():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        longitude = request.form.get('longitude')
-        latitude = request.form.get('latitude')
+        data = request.get_json()
 
+        longitude = data.get('longitude')
+        latitude = data.get('latitude')
+        
         result = __get_next_acquisition_date(longitude, latitude)
         img = f'<img src="/plot.png?long={longitude}&lat={latitude}" alt="Generated Plot">'
 
-        return render_template('index.html', result=result, img=img)
+        rendered_table = render_template('table.html', result=result, img=img)
+        return jsonify({'html': rendered_table})
+
     return render_template('index.html', result=None, img=None)
+
+@app.route('/set_notification', methods=['POST'])
+def set_notification():
+    data = request.get_json()
+
+    days = data.get('days')
+    date = data.get('date')
+    satellite = data.get('satellite')
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+
+
+    # Here, implement your logic to save the notification.
+    if save_notification(days, date, satellite, latitude, longitude):
+        return jsonify({'message': 'Notification set successfully!'}), 200
+    else:
+        return jsonify({'message': 'Failed to set notification!'}), 500
+
+def save_notification(days, date, satellite, latitude, longitude):
+    return True
 
 def __get_next_acquisition_date(longitude, latitude):
     global wrs2, schedule
@@ -44,7 +72,7 @@ def __get_next_acquisition_date(longitude, latitude):
     landsat9_schedules = schedule.get_next_acquisition_dates('landsat_9', path)
 
     next_acq_dates = {}
-    
+
     for date in landsat8_schedules:
         next_acq_dates[date] = 'Landsat 8'
 
@@ -55,8 +83,10 @@ def __get_next_acquisition_date(longitude, latitude):
 
     for date, sat in next_acq_dates.items():
         html += '<tr>\n'
-        html += f"<td>{date}</td>\n"
+        html += f"<td style=\"text-align: left;\">{date}</td>\n"
         html += f"<td>{sat}</td>\n"
+        html += f"<td><a href='#' data-date=\"{date}\"\
+        data-satellite=\"{sat}\" class=\"popupButton\">Notify Me!</a></td>\n"
         html += '</tr>\n'
 
     return html
